b7b8a050240a26092068bde8b5900dbd625f761d
SQReder <sqreder@gmail.com>
2019-11-29 21:03:55 +0300

    Updates

---
+++ precache-manifest.43e4cb2257aafabbe48688e1644ba9f2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3c08aec159f975f8bb8bb2db58ac18f",
+    "url": "/menu-planner/index.html"
+  },
+  {
+    "revision": "98db7ccc37790cde41ca",
+    "url": "/menu-planner/static/css/main.e1305a31.chunk.css"
+  },
+  {
+    "revision": "bfc67516963137b3a182",
+    "url": "/menu-planner/static/js/2.358e4318.chunk.js"
+  },
+  {
+    "revision": "98db7ccc37790cde41ca",
+    "url": "/menu-planner/static/js/main.efa2f461.chunk.js"
+  },
+  {
+    "revision": "cb7f35da72f7861d89ae",
+    "url": "/menu-planner/static/js/runtime-main.c2eb49ef.js"
+  }
+]);
